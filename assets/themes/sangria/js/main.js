bde67eac1349b3eac965de361ebb18b5519b5a68
Justin Long <justinlong@outlook.com>
2017-04-03 10:19:01 -0700

    Add limit.

--- assets/themes/sangria/js/main.js
+++ assets/themes/sangria/js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
   // instagram feed
   var feed = new Instafeed({
+      clientId: 'babb8453913c4c7f87f210172eed1166',
       get: 'user',
       userId: 'meditationsontech'
-      clientId: 'babb8453913c4c7f87f210172eed1166',
+      limit: 7,
       resolution: 'thumbnail',
       template: '<div class="footer-instagram-el tn-col-7">' +
                 '<a class="footer-instagram-link cursor-zoom" href="{{link}}"><img alt="{{post.title}}" '+ 'src="{{image}}"></a>'+
